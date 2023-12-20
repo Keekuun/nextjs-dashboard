ceea8e1c54ac55157113a0cc8845869e798dc5d5
jeek <zhoukekun@xunlei.com>
2023-12-20 17:05:01 +0800

    fix: fix 'invoice' is possibly 'undefined'.

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -16,7 +16,7 @@ export default function EditInvoiceForm({
   invoice,
   customers,
 }: {
-  invoice: InvoiceForm | undefined;
+  invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
   const initialState = { message: null, errors: {} };
@@ -36,7 +36,7 @@ export default function EditInvoiceForm({
               id="customer"
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-              defaultValue={invoice?.customer_id}
+              defaultValue={invoice.customer_id}
               aria-describedby="customer-error"
             >
               <option value="" disabled>
