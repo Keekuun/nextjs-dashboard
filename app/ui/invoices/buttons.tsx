0d0df34cc7772631d05b221f7db1845b3073bd5b
jeek <zkkysqs@gmail.com>
2025-07-02 14:53:41 +0800

    fix(invoices): 修改删除发票按钮的服务器动作处理方式

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,7 +26,10 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  async function deleteInvoiceWithId() {
+    'use server';
+    await deleteInvoice(id)
+  }
   return (
     <form action={deleteInvoiceWithId}>
       <button className="rounded-md border p-2 hover:bg-gray-100">
